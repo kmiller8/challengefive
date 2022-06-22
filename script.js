00922dc0c964b708e18cd415fab5863bb462e6fb
kmiller8 <kmiller@rpm.com>
2022-06-22 18:05:05 -0400

    adding html css and javascript

---
+++ script.js
@@ -0,0 +1,66 @@
+
+//stores current date in variable
+var date = moment().format('MMMM Do YYYY');
+
+//displays date
+$("#currentDay").text(date);
+
+//stores the hours
+var hours = moment().hours();
+
+//saves text when clicking on save button
+$(".saveBtn").on("click", function(){
+
+    //stores which location the text is
+    var time = $(this).siblings(".hours").text();
+
+    //stores what the text is
+    var descriptionText = $(this).siblings(".description").val();
+    
+    //puts it in local storage
+    localStorage.setItem(time, descriptionText);
+    
+    });
+
+
+function getPlans (){
+$(".hours").each(function(){
+
+    var plannedHour = $(this).text();
+    var plans = localStorage.getItem(plannedHour);
+
+    $(this).siblings(".description").val(plans);
+
+})
+};
+
+function hourColor () {
+    
+   
+    console.log(hours)
+
+    $(".time-block").each(function () {
+        
+
+        var currentHour = parseInt($(this).attr("time"));
+        console.log(currentHour);
+
+        if (currentHour == hours) {
+            $(this).addClass("present");
+
+
+        } 
+        else if (currentHour > hours) {
+            $(this).addClass("future");
+        
+        } 
+        
+        else {
+            $(this).addClass("past");
+        
+        }
+    })
+};
+
+getPlans();
+hourColor();
